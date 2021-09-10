e4b4a032bf0fbd16c29c3c905806006976b9ce70
RANAJITPAL <palranajit98@gmail.com>
2021-09-10 11:33:38 +0530

    Initial Commit

---
+++ src/test/java/com/stock_analyzer/stock_analyzer/StockAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_analyzer.stock_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
